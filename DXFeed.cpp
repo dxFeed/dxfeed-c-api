b0d34231197ed15250f9f3b27f916d764f7f8b02
Dmitry Tatubaev <tatubaev@devexperts.com>
2010-08-12 14:30:42 +0000

    [DXF-12] QD messages parsing - data message
    git-svn-id: https://svn.in.devexperts.com/DXF/trunk@7 f4d0cfa4-2545-4b7a-9b12-d45f792d56f8

--- DXFeed.cpp
+++ DXFeed.cpp
@@ -1,8 +1,24 @@
-// DXFeed.cpp : Defines the entry point for the DLL application.
-//
+/*
+* The contents of this file are subject to the Mozilla Public License Version
+* 1.1 (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+*
+* Software distributed under the License is distributed on an "AS IS" basis,
+* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+* for the specific language governing rights and limitations under the
+* License.
+*
+* The Initial Developer of the Original Code is Devexperts LLC.
+* Portions created by the Initial Developer are Copyright (C) 2010
+* the Initial Developer. All Rights Reserved.
+*
+* Contributor(s):
+*
+*/
 
-#include "stdafx.h"
 #include "DXFeed.h"
+
 BOOL APIENTRY DllMain( HANDLE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved )
 {
 	switch (ul_reason_for_call)
@@ -14,20 +30,4 @@ BOOL APIENTRY DllMain( HANDLE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 		break;
 	}
     return TRUE;
-}
-
-// This is an example of an exported variable
-DXFEED_API int nDXFeed=0;
-
-// This is an example of an exported function.
-DXFEED_API int fnDXFeed(void)
-{
-	return 42;
-}
-
-// This is the constructor of a class that has been exported.
-// see DXFeed.h for the class definition
-CDXFeed::CDXFeed()
-{ 
-	return; 
 }
