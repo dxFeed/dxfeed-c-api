fb7ec80448ec754b111c9cc1ff41b9812bb20988
Dmitry Tatubaev <tatubaev@devexperts.com>
2010-10-08 14:19:15 +0000

    [DXF-25] Samples
    git-svn-id: https://svn.in.devexperts.com/DXF/trunk@137 f4d0cfa4-2545-4b7a-9b12-d45f792d56f8

---
+++ tests/FullTest/FullTest.c
@@ -0,0 +1,471 @@
+#include "DXFeed.h"
+#include "DXErrorCodes.h"
+#include "Logger.h"
+#include <stdio.h>
+#include <Windows.h>
+
+const char dxfeed_host[] = "demo.dxfeed.com:7300";
+HANDLE g_out_console;
+
+dx_const_string_t dx_event_type_to_string (int event_type) {
+    switch (event_type) {
+    case DXF_ET_TRADE: return L"Trade";
+    case DXF_ET_QUOTE: return L"Quote";
+    case DXF_ET_SUMMARY: return L"Summary";
+    case DXF_ET_PROFILE: return L"Profile";
+    case DXF_ET_ORDER: return L"Order";
+    case DXF_ET_TIME_AND_SALE: return L"Time&Sale";
+    default: return L"";
+    }
+}
+
+#define SYMBOLS_COUNT 4
+static const dx_const_string_t g_symbols[] = { {L"IBM"}, {L"MSFT"}, {L"YHOO"}, {L"C"} };
+//static const dx_int_t g_symbols_size = sizeof (g_symbols) / sizeof (g_symbols[0]);
+
+/* -------------------------------------------------------------------------- */
+void trade_listener (int event_type, dx_const_string_t symbol_name, const dx_event_data_t* data, int data_count);
+void quote_listener (int event_type, dx_const_string_t symbol_name, const dx_event_data_t* data, int data_count);
+void summary_listener (int event_type, dx_const_string_t symbol_name, const dx_event_data_t* data, int data_count);
+void profile_listener (int event_type, dx_const_string_t symbol_name, const dx_event_data_t* data, int data_count);
+void order_listener (int event_type, dx_const_string_t symbol_name, const dx_event_data_t* data, int data_count);
+void time_and_sale_listener (int event_type, dx_const_string_t symbol_name, const dx_event_data_t* data, int data_count);
+
+struct event_info_t {
+    dx_event_id_t       id;
+    int                 event_type;
+    COORD               coord;
+    dx_event_listener_t listener;
+    unsigned int        total_data_count[SYMBOLS_COUNT];
+};
+
+static struct event_info_t event_info[dx_eid_count] = { {dx_eid_trade, DXF_ET_TRADE, {0, 0}, trade_listener, 0},
+{dx_eid_quote, DXF_ET_QUOTE, {0, 6}, quote_listener, 0},
+{dx_eid_summary, DXF_ET_SUMMARY, {0, 12}, summary_listener, 0},
+{dx_eid_profile, DXF_ET_PROFILE, {0, 18}, profile_listener, 0},
+{dx_eid_order, DXF_ET_ORDER, {0, 24}, order_listener, 0},
+{dx_eid_time_and_sale, DXF_ET_TIME_AND_SALE, {0, 30}, time_and_sale_listener, 0}};
+
+/* -------------------------------------------------------------------------- */
+
+static bool is_listener_thread_terminated = false;
+CRITICAL_SECTION listener_thread_guard;
+
+bool is_thread_terminate() {
+    bool res;
+    EnterCriticalSection(&listener_thread_guard);
+    res = is_listener_thread_terminated;
+    LeaveCriticalSection(&listener_thread_guard);
+
+    return res;
+}
+
+/* -------------------------------------------------------------------------- */
+
+void print_at(COORD c, wchar_t* str) {
+    DWORD dummy;
+    SetConsoleCursorPosition(g_out_console, c);
+    WriteConsoleW(g_out_console, str, (DWORD)wcslen(str), &dummy, NULL);
+
+}
+
+void on_reader_thread_terminate(const char* host ) {
+    EnterCriticalSection(&listener_thread_guard);
+    is_listener_thread_terminated = true;
+    LeaveCriticalSection(&listener_thread_guard);
+
+    printf("\nTerminating listener thread, host: %s\n", host);
+}
+
+/* -------------------------------------------------------------------------- */
+
+int get_symbol_index(dx_const_string_t symbol_name) {
+    int i = 0;
+    for (; i < SYMBOLS_COUNT; ++i) {
+        if (wcsicmp(symbol_name, g_symbols[i]) == 0) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+/* -------------------------------------------------------------------------- */
+
+void trade_listener (int event_type, dx_const_string_t symbol_name, const dx_event_data_t* data, int data_count) {
+    dx_int_t i = 0;
+    wchar_t str[200];
+    int ind;
+    COORD coord = event_info[dx_eid_trade].coord;
+
+    if (event_type != DXF_ET_TRADE) {
+        swprintf(str, L"Error: event: %s Symbol: %s, expected event: Trade\n",dx_event_type_to_string(event_type), symbol_name);
+        print_at( coord, str);
+        return;
+    }
+
+    print_at( coord, L"Event Trade:");
+    ind = get_symbol_index(symbol_name);
+    if (ind == -1) {
+        return;
+    }
+
+    event_info[dx_eid_trade].total_data_count[ind] += data_count;
+
+    coord.X += 5;
+    coord.Y += ind + 1;
+    swprintf(str, L"Symbol: \"%s\" Data count: %d, Total data count: %d            ", symbol_name, data_count, event_info[dx_eid_trade].total_data_count[ind]);
+    print_at( coord, str);
+    //swprintf(str, L"Event: %s Symbol: %s Data count: %d, Total data count: %d            ",dx_event_type_to_string(event_type), symbol_name, data_count, event_info[dx_eid_trade].total_data_count);
+    //print_at( event_info[dx_eid_trade].coord, str);
+}
+
+/* -------------------------------------------------------------------------- */
+
+void quote_listener (int event_type, dx_const_string_t symbol_name, const dx_event_data_t* data, int data_count) {
+    dx_int_t i = 0;
+    wchar_t str[200];
+    int ind;
+    COORD coord = event_info[dx_eid_quote].coord;
+
+    if (event_type != DXF_ET_QUOTE) {
+        swprintf(str, L"Error: event: %s Symbol: %s, expected event: Quote\n",dx_event_type_to_string(event_type), symbol_name);
+        print_at( coord, str);
+        return;
+    }
+
+    print_at( coord, L"Event Quote:");
+    ind = get_symbol_index(symbol_name);
+    if (ind == -1) {
+        return;
+    }
+
+    event_info[dx_eid_quote].total_data_count[ind] += data_count;
+
+    coord.X += 5;
+    coord.Y += ind + 1;
+    swprintf(str, L"Symbol: \"%s\" Data count: %d, Total data count: %d            ", symbol_name, data_count, event_info[dx_eid_quote].total_data_count[ind]);
+    print_at( coord, str);
+
+    //event_info[dx_eid_quote].total_data_count += data_count;
+    //swprintf(str, L"Event: %s Symbol: %s Data count: %d, Total data count: %d            ",dx_event_type_to_string(event_type), symbol_name, data_count, event_info[dx_eid_quote].total_data_count);
+    //print_at( event_info[dx_eid_quote].coord, str);
+}
+
+/* -------------------------------------------------------------------------- */
+
+void summary_listener (int event_type, dx_const_string_t symbol_name, const dx_event_data_t* data, int data_count) {
+    dx_int_t i = 0;
+    wchar_t str[200];
+    int ind;
+    COORD coord = event_info[dx_eid_summary].coord;
+
+    if (event_type != DXF_ET_SUMMARY) {
+        swprintf(str, L"Error: event: %s Symbol: %s, expected event: Summary\n",dx_event_type_to_string(event_type), symbol_name);
+        print_at( coord, str);
+        return;
+    }
+
+    print_at( coord, L"Event Summary:");
+    ind = get_symbol_index(symbol_name);
+    if (ind == -1) {
+        return;
+    }
+
+    event_info[dx_eid_summary].total_data_count[ind] += data_count;
+
+    coord.X += 5;
+    coord.Y += ind + 1;
+    swprintf(str, L"Symbol: \"%s\" Data count: %d, Total data count: %d            ", symbol_name, data_count, event_info[dx_eid_summary].total_data_count[ind]);
+    print_at( coord, str);
+    //event_info[dx_eid_summary].total_data_count += data_count;
+    //swprintf(str, L"Event: %s Symbol: %s Data count: %d, Total data count: %d            ",dx_event_type_to_string(event_type), symbol_name, data_count, event_info[dx_eid_summary].total_data_count);
+    //print_at( event_info[dx_eid_summary].coord, str);
+}
+
+/* -------------------------------------------------------------------------- */
+
+void profile_listener (int event_type, dx_const_string_t symbol_name, const dx_event_data_t* data, int data_count) {
+    dx_int_t i = 0;
+    wchar_t str[200];
+    int ind;
+    COORD coord = event_info[dx_eid_profile].coord;
+
+    if (event_type != DXF_ET_PROFILE) {
+        swprintf(str, L"Error: event: %s Symbol: %s, expected event: Profile\n",dx_event_type_to_string(event_type), symbol_name);
+        print_at( coord, str);
+        return;
+    }
+
+    print_at( coord, L"Event Profile:");
+    ind = get_symbol_index(symbol_name);
+    if (ind == -1) {
+        return;
+    }
+
+    event_info[dx_eid_profile].total_data_count[ind] += data_count;
+
+    coord.X += 5;
+    coord.Y += ind + 1;
+    swprintf(str, L"Symbol: \"%s\" Data count: %d, Total data count: %d            ", symbol_name, data_count, event_info[dx_eid_profile].total_data_count[ind]);
+    print_at( coord, str);
+    //event_info[dx_eid_profile].total_data_count += data_count;
+    //swprintf(str, L"Event: %s Symbol: %s Data count: %d, Total data count: %d            ",dx_event_type_to_string(event_type), symbol_name, data_count, event_info[dx_eid_profile].total_data_count);
+    //print_at( event_info[dx_eid_profile].coord, str);
+}
+
+/* -------------------------------------------------------------------------- */
+
+void order_listener (int event_type, dx_const_string_t symbol_name, const dx_event_data_t* data, int data_count) {
+    dx_int_t i = 0;
+    wchar_t str[200];
+    int ind;
+    COORD coord = event_info[dx_eid_order].coord;
+
+    if (event_type != DXF_ET_ORDER) {
+        swprintf(str, L"Error: event: %s Symbol: %s, expected event: Order\n",dx_event_type_to_string(event_type), symbol_name);
+        print_at( coord, str);
+        return;
+    }
+
+    print_at( coord, L"Event Order:");
+    ind = get_symbol_index(symbol_name);
+    if (ind == -1) {
+        return;
+    }
+
+    event_info[dx_eid_order].total_data_count[ind] += data_count;
+
+    coord.X += 5;
+    coord.Y += ind + 1;
+    swprintf(str, L"Symbol: \"%s\" Data count: %d, Total data count: %d            ", symbol_name, data_count, event_info[dx_eid_order].total_data_count[ind]);
+    print_at( coord, str);
+    //event_info[dx_eid_order].total_data_count += data_count;
+    //swprintf(str, L"Event: %s Symbol: %s Data count: %d, Total data count: %d            ",dx_event_type_to_string(event_type), symbol_name, data_count, event_info[dx_eid_order].total_data_count);
+    //print_at( event_info[dx_eid_order].coord, str);
+}
+
+/* -------------------------------------------------------------------------- */
+
+void time_and_sale_listener (int event_type, dx_const_string_t symbol_name, const dx_event_data_t* data, int data_count) {
+    dx_int_t i = 0;
+    wchar_t str[200];
+    int ind;
+    COORD coord = event_info[dx_eid_time_and_sale].coord;
+
+    if (event_type != DXF_ET_TIME_AND_SALE) {
+        swprintf(str, L"Error: event: %s Symbol: %s, expected event: Time&Sale\n",dx_event_type_to_string(event_type), symbol_name);
+        print_at( coord, str);
+        return;
+    }
+
+    print_at( coord, L"Event Time&Sale");
+    ind = get_symbol_index(symbol_name);
+    if (ind == -1) {
+        return;
+    }
+
+    event_info[dx_eid_time_and_sale].total_data_count[ind] += data_count;
+
+    coord.X += 5;
+    coord.Y += ind + 1;
+    swprintf(str, L"Symbol: \"%s\" Data count: %d, Total data count: %d            ", symbol_name, data_count, event_info[dx_eid_time_and_sale].total_data_count[ind]);
+    print_at( coord, str);
+    //event_info[dx_eid_time_and_sale].total_data_count += data_count;
+    //swprintf(str, L"Event: %s Symbol: %s Data count: %d, Total data count: %d            ",dx_event_type_to_string(event_type), symbol_name, data_count, event_info[dx_eid_time_and_sale].total_data_count);
+    //print_at( event_info[dx_eid_time_and_sale].coord, str);
+}
+
+/* -------------------------------------------------------------------------- */
+
+void process_last_error () {
+    int subsystem_id = dx_sc_invalid_subsystem;
+    int error_code = DX_INVALID_ERROR_CODE;
+    dx_const_string_t error_descr = NULL;
+    int res;
+
+    res = dxf_get_last_error(&subsystem_id, &error_code, &error_descr);
+
+    if (res == DXF_SUCCESS) {
+        if (subsystem_id == dx_sc_invalid_subsystem && error_code == DX_INVALID_ERROR_CODE) {
+            printf("WTF - no error information is stored");
+
+            return;
+        }
+
+        wprintf(L"Error occurred and successfully retrieved:\n"
+            L"subsystem code = %d, error code = %d, description = \"%s\"\n",
+            subsystem_id, error_code, error_descr);
+        return;
+    }
+
+    printf("An error occurred but the error subsystem failed to initialize\n");
+}
+
+dxf_subscription_t create_subscription(dxf_connection_t connection, int event_id, dx_event_listener_t listener) {
+    dxf_subscription_t subscription;
+    wchar_t str[100];
+    const int event_type = event_info[event_id].event_type;
+    int i;
+    COORD coord = event_info[event_id].coord;
+
+    swprintf(str, L"There is no data for event: %s", dx_event_type_to_string(event_type));
+    print_at( coord, str);
+
+    if (!dxf_create_subscription(connection, event_type, &subscription)) {
+        process_last_error();
+
+        return NULL;
+    };
+
+    for (i = 0; i < SYMBOLS_COUNT; ++i) {
+        int ind;
+        COORD symbol_coord = coord;
+        if (!dxf_add_symbol(subscription, g_symbols[i])) {
+            process_last_error();
+
+            return NULL;
+        };
+
+        swprintf(str, L"Symbol %s: no data", g_symbols[i]);
+        ind = get_symbol_index(g_symbols[i]);
+        if (ind == -1) {
+            return NULL;
+        }
+        symbol_coord.Y += ind + 1;
+        print_at(symbol_coord, str);
+    }
+
+    if (!dxf_attach_event_listener(subscription, listener)) {
+        process_last_error();
+
+        return NULL;
+    };
+
+    return subscription;
+}
+/* -------------------------------------------------------------------------- */
+
+bool initialize_console() {
+    CONSOLE_SCREEN_BUFFER_INFO info;
+    COORD c = {80, 40};
+    SMALL_RECT rect = {0, 0, 79, 39};
+
+    g_out_console = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (g_out_console == NULL) {
+        return false;
+    }
+
+    if (!GetConsoleScreenBufferInfo(g_out_console, &info)) {
+        return false;
+    }
+
+    if (!SetConsoleWindowInfo(g_out_console, TRUE, &rect)) {
+        return false;
+    }
+
+    if (info.dwSize.X >= 80 && info.dwSize.Y >= 40) {
+        return true;
+    }
+
+    if (!SetConsoleScreenBufferSize(g_out_console, c)) {
+        return false;
+    }
+
+    return true;
+}
+
+int main (int argc, char* argv[]) {
+    dxf_connection_t connection;
+    dxf_subscription_t subscriptions[dx_eid_count];
+    //dxf_subscription_t trade_subscription;
+    //dxf_subscription_t quote_subscription;
+    //dxf_subscription_t summary_subscription;
+    //dxf_subscription_t profile_subscription;
+    //dxf_subscription_t order_subscription;
+    //dxf_subscription_t time_and_sale_subscription;
+    int loop_counter = 10000;
+    int i;
+    
+    if (!initialize_console()) {
+        return -1;
+    }
+
+    dxf_initialize_logger( "log.log", true, true, true );
+
+    //printf("Sample test started.\n");    
+    //printf("Connecting to host %s...\n", dxfeed_host);
+
+    if (!dxf_create_connection(dxfeed_host, on_reader_thread_terminate, &connection)) {
+        process_last_error();
+        return -1;
+    }
+
+    //printf("Connection successful!\n");
+
+    // create subscriptions
+    for (i = dx_eid_begin; i < dx_eid_count; ++i) {
+        subscriptions[i] = create_subscription(connection, i, event_info[i].listener);
+        if (subscriptions[i] == NULL) {
+            return -1;
+        }
+    }
+    //// trade subscription
+    //trade_subscription = create_subscription(connection, DXF_ET_TRADE, trade_listener);
+    //if (trade_subscription == NULL) {
+    //    return -1;
+    //}
+
+    //// quote subscription
+    //quote_subscription = create_subscription(connection, DXF_ET_QUOTE, quote_listener);
+    //if (quote_subscription == NULL) {
+    //    return -1;
+    //}
+
+    //// summary subscription
+    //summary_subscription = create_subscription(connection, DXF_ET_SUMMARY, summary_listener);
+    //if (summary_subscription == NULL) {
+    //    return -1;
+    //}
+
+    //// profile subscription
+    //profile_subscription = create_subscription(connection, DXF_ET_PROFILE, profile_listener);
+    //if (profile_subscription == NULL) {
+    //    return -1;
+    //}
+
+    //// order subscription
+    //order_subscription = create_subscription(connection, DXF_ET_ORDER, order_listener);
+    //if (order_subscription == NULL) {
+    //    return -1;
+    //}
+
+    //// time_and_sale subscription
+    //time_and_sale_subscription = create_subscription(connection, DXF_ET_TIME_AND_SALE, time_and_sale_listener);
+    //if (time_and_sale_subscription == NULL) {
+    //    return -1;
+    //}
+
+    //printf("Subscriptions successful!\n");
+
+    // main loop
+    InitializeCriticalSection(&listener_thread_guard);
+    while (!is_thread_terminate() && loop_counter--) {
+        Sleep(100);
+    }
+    DeleteCriticalSection(&listener_thread_guard);
+
+    printf("Disconnecting from host...\n");
+
+    if (!dxf_close_connection(connection)) {
+        process_last_error();
+
+        return -1;
+    }
+
+    printf("Disconnect successful!\n"
+        "Connection test completed successfully!\n");
+
+    return 0;
+}
+
