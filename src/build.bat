049b8ef12ed294b89033ac850d1336f9b08cea63
Nikita Avdeev <nikita.avdeev@simbirsoft.com>
2016-04-04 11:30:54 +0400

    Update CMakeLists and build script

--- src/build.bat
+++ src/build.bat
@@ -1,49 +1,52 @@
 @echo off
+
 set BUILD_DIR=build
 set CONFIG=
 set PLATFORM=
+set CMAKE_PLATFORM=
 
-if [%*] EQU 0 (
-    goto build
-) else if [%*] GTR 2 (
-    goto usage
-) else (
-    for %%A in (%*) do (
-        if %%A EQU "Debug" (
-            if %CONGIG% NEQ "" goto usage
-            set CONFIG=%%A
-        ) else if %%A EQU "Release" (
-            if %CONGIG% NEQ "" goto usage
-            set CONFIG=%%A
-        ) else if %%A EQU "x86" (
-            if %PLATFORM% NEQ "" goto usage
-            set PLATFORM=%%A
-        ) else if %%A EQU "x64" (
-            if %PLATFORM% NEQ "" goto usage
-            set PLATFORM=%%A
-        )
+for %%A in (%*) do (
+    if [%%A] EQU [Debug] (
+        set CONFIG=%%A
+    ) else if [%%A] EQU [Release] (
+        set CONFIG=%%A
+    ) else if [%%A] EQU [x86] (
+        set PLATFORM=%%A
+    ) else if [%%A] EQU [x64] (
+        set PLATFORM=%%A
+        set CMAKE_PLATFORM=" Win64"
+    ) else (
+        goto usage
     )
 )
-if %CONGIG% EQU "" set CONFIG=Release
-if %PLATFORM% EQU "" set PLATFORM=x86
-
-echo Start building %CONFIG% %PLATFORM%...
-exit 1
 
 :build
+if [%CONFIG%] EQU [] set CONFIG=Release
+if [%PLATFORM%] EQU [] set PLATFORM=x86
+
 if not exist %BUILD_DIR% (mkdir %BUILD_DIR%)
 cd %BUILD_DIR%
 
-REM cmake -DCMAKE_BUILD_TYPE=Debug -G "Visual Studio 10 2010" ..
+echo Start building %CONFIG% %PLATFORM%...
 
-cmake -DCMAKE_BUILD_TYPE=Debug -G "NMake Makefiles" ..
+cmake -DCMAKE_BUILD_TYPE=%CONFIG% -G "NMake Makefiles%CMAKE_PLATFORM%" ..
+if %ERRORLEVEL% GEQ 1 goto build_error
 
+nmake
+if %ERRORLEVEL% GEQ 1 goto build_error
+
+:build_success
 cd..
-echo Build success!
-exit 1
+echo Build success.
+goto end
+:build_error
+cd..
+echo Build error!
+goto end
 
 :usage
 echo Usage: %0 [configuration] [platform]
 echo        configuration - Debug or Release
 echo        platform      - x86 or x64
-exit 1
+
+:end
