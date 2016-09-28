76028bcb6716164b2a117e0d4773a892f6f48bce
Nikita Avdeev <nikita.avdeev@simbirsoft.com>
2016-09-28 18:51:30 +0400

    Update SnapshotConsoleSample cmake build script

--- tests/SnapshotConsoleSample/build.bat
+++ tests/SnapshotConsoleSample/build.bat
@@ -4,6 +4,7 @@ set BUILD_DIR=build
 set CONFIG=
 set PLATFORM=
 set CMAKE_PLATFORM=
+set MSBUILD_PLATFORM=
 
 for %%A in (%*) do (
     if [%%A] EQU [Debug] (
@@ -12,9 +13,22 @@ for %%A in (%*) do (
         set CONFIG=%%A
     ) else if [%%A] EQU [x86] (
         set PLATFORM=%%A
+        set MSBUILD_PLATFORM=Win32
     ) else if [%%A] EQU [x64] (
         set PLATFORM=%%A
-        set CMAKE_PLATFORM=" Win64"
+        set CMAKE_PLATFORM= Win64
+        set MSBUILD_PLATFORM=x64
+    ) else if [%%A] EQU [clean] (
+        if exist %BUILD_DIR% (
+            RMDIR /S /Q %BUILD_DIR%
+            if %ERRORLEVEL% GEQ 1 goto exit_error
+        )
+        goto exit_success
+    ) else if [%%A] EQU [rebuild] (    
+        if exist %BUILD_DIR% (
+            RMDIR /S /Q %BUILD_DIR%
+            if %ERRORLEVEL% GEQ 1 goto exit_error
+        )
     ) else (
         goto usage
     )
@@ -23,31 +37,37 @@ for %%A in (%*) do (
 :build
 if [%CONFIG%] EQU [] set CONFIG=Release
 if [%PLATFORM%] EQU [] set PLATFORM=x86
+set BUILD_DIR=%BUILD_DIR%\%PLATFORM%
 
 if not exist %BUILD_DIR% (mkdir %BUILD_DIR%)
 cd %BUILD_DIR%
 
 echo Start building %CONFIG% %PLATFORM%...
-
-rem cmake -DCMAKE_BUILD_TYPE=%CONFIG% -G "Visual Studio 10 2010%CMAKE_PLATFORM%" ..
-cmake -DCMAKE_BUILD_TYPE=%CONFIG% -G "NMake Makefiles%CMAKE_PLATFORM%" ..
+cmake -DCMAKE_BUILD_TYPE=%CONFIG% -G "Visual Studio 10 2010%CMAKE_PLATFORM%" -DTARGET_PLATFORM=%PLATFORM% ..\..
 if %ERRORLEVEL% GEQ 1 goto build_error
 
-nmake
+msbuild /p:Configuration=%CONFIG% /p:Platform=%MSBUILD_PLATFORM% /m /t:build /ignore:.vcproj,.vcxproj
 if %ERRORLEVEL% GEQ 1 goto build_error
 
 :build_success
-cd..
+cd..\..
 echo Build success.
-goto end
+goto exit_success
+
 :build_error
-cd..
+cd..\..
 echo Build error!
-goto end
+goto exit_error
 
 :usage
-echo Usage: %0 [configuration] [platform]
-echo        configuration - Debug or Release
-echo        platform      - x86 or x64
+echo Usage: %0 [^<configuration^>] [^<platform^>] [rebuild^|clean]
+echo        ^<configuration^> - Debug or Release
+echo        ^<platform^>      - x86 or x64
+echo        clean           - removes build directory
+echo        rebuild         - performs clean and build
+goto exit_error
 
-:end
+:exit_success
+exit /b 0
+:exit_error
+exit /b 1
