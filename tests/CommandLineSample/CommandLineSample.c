1da6818b003a00f7c930b21735ac8301fa4afa70
nikita.avdeev <nikita.avdeev@simbirsoft.com>
2016-10-27 17:40:42 +0400

    minor changes

--- tests/CommandLineSample/CommandLineSample.c
+++ tests/CommandLineSample/CommandLineSample.c
@@ -126,7 +126,7 @@ void listener(int event_type, dxf_const_string_t symbol_name,
     }
     
     if (event_type == DXF_ET_TRADE) {
-        dxf_trade_t* trades = (dx_trade_t*)data;
+        dxf_trade_t* trades = (dxf_trade_t*)data;
 
         for (; i < data_count; ++i) {
             print_timestamp(trades[i].time);
