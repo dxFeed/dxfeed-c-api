1ea510b44e9fd3c7e638463d096eb7e95c722b5a
nikita.avdeev <nikita.avdeev@simbirsoft.com>
2016-11-24 10:59:03 +0400

    Migrate VC++2010 projects to VC++2015

--- tests/APITest/APITest.c
+++ tests/APITest/APITest.c
@@ -73,7 +73,7 @@ void first_listener(int event_type, dxf_const_string_t symbol_name,
         dxf_trade_t* trades = (dx_trade_t*)data;
 
         for (; i < data_count; ++i) {
-            wprintf(L"time=%i, exchange code=%C, price=%f, size=%i, tick=%i, change=%f, day volume=%i\n",
+            wprintf(L"time=%i, exchange code=%C, price=%f, size=%i, tick=%lld, change=%f, day volume=%f\n",
                 (int)trades[i].time, trades[i].exchange_code, trades[i].price, (int)trades[i].size, trades[i].tick,
                 trades[i].change, trades[i].day_volume);
         }
@@ -107,7 +107,7 @@ void first_listener(int event_type, dxf_const_string_t symbol_name,
         dxf_time_and_sale_t* tns = (dxf_time_and_sale_t*)data;
 
         for (; i < data_count ; ++i) {
-            wprintf(L"event id=%ld, time=%ld, exchange code=%c, price=%f, size=%li, bid price=%f, ask price=%f, "
+            wprintf(L"event id=%lld, time=%lld, exchange code=%c, price=%f, size=%li, bid price=%f, ask price=%f, "
                 L"exchange sale conditions=%s, is trade=%s, type=%i\n",
                 tns[i].event_id, tns[i].time, tns[i].exchange_code, tns[i].price, (int)tns[i].size,
                 tns[i].bid_price, tns[i].ask_price, tns[i].exchange_sale_conditions,
