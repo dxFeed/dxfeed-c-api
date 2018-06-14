d30033c6f22a6ea6437b6101c305173f86b8f70d
Lev Serebryakov <serebryakov@devexperts.com>
2018-06-14 15:37:56 +0300

    Release 5.2.3

--- src/Version.h
+++ src/Version.h
@@ -28,7 +28,7 @@
 
 #define DX_VERSION_MAJOR 5
 #define DX_VERSION_MINOR 2
-#define DX_VERSION_PATCH 2
+#define DX_VERSION_PATCH 3
 
 #define DX_VER_FILE_VERSION         DX_VERSION_MAJOR, DX_VERSION_MINOR, DX_VERSION_PATCH
 #define DX_VER_FILE_VERSION_STR     DX_TO_STRING(DX_VERSION_MAJOR) \
@@ -48,7 +48,7 @@
 #define DX_VER_LEGAL_COPYRIGHT   "Copyright (C) 2010-2018 Devexperts LLC"
 #define DX_VER_PRODUCT_NAME      "DXFeed C API"
 
-#define DX_LIBRARY_VERSION L"DXFeed.cpp v 5.2.2 (c) Devexperts"
+#define DX_LIBRARY_VERSION L"DXFeed.cpp v 5.2.3 (c) Devexperts"
 
 extern dxf_const_string_t DX_LIBRARY_OPTIONS;
 
