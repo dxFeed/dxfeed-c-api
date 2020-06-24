d2e6eb25a950ba8d8e0147121b12248a8e49c1e5
ttldtor <ttldtor@gmail.com>
2020-06-24 19:06:10 +0300

    dxf_initialize_logger_v2

--- wrappers/cpp/samples/ConnectionSample/main.cpp
+++ wrappers/cpp/samples/ConnectionSample/main.cpp
@@ -49,7 +49,7 @@ int main(int argc, char **argv) {
 	auto address = args::get(addressParameter);
 	auto symbols = args::get(symbolsParameter);
 
-	dxf_initialize_logger("ConnectionSample.log", true, true, true, false);
+	dxf_initialize_logger("ConnectionSample.log", true, true, true);
 	std::cout << "Connecting...\n";
 	//auto connection = Connection::create("demo.dxfeed.com:7300");
 	auto connection = Connection::create(address);
