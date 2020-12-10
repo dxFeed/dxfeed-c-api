f32cbd4df891cd92ff39479d2d5c4dcc670519a6
ttldtor <ttldtor@gmail.com>
2020-12-10 18:35:05 +0300

    [DXFC-200] [Linux][No TLS] Add build a static version of the library

--- .github/actions/centos/entrypoint.sh
+++ .github/actions/centos/entrypoint.sh
@@ -21,12 +21,3 @@ mkdir -p artifact
 source scl_source enable devtoolset-2
 ./make_package.sh $RELEASE rebuild no-test $NO_TLS_FLAG
 mv ./build/dxfeed-c-api-$RELEASE${NO_TLS_SUFFIX}.zip ./artifact/$ARTIFACT.zip
-
-#ARCH_NAME=dxfeed-c-api-$BUILD_VERSION-centos
-#git clone https://github.com/dxFeed/dxfeed-c-api.git $SRC_PATH                 && \
-#source scl_source enable devtoolset-2                                          && \
-#cd $SRC_PATH                                                                   && \
-#./make_package.sh $BUILD_VERSION rebuild no-test no-tls                        && \
-#cp ./build/dxfeed-c-api-$BUILD_VERSION-no-tls.zip /build/$ARCH_NAME-no-tls.zip && \
-#./make_package.sh $BUILD_VERSION rebuild no-test                               && \
-#cp ./build/dxfeed-c-api-$BUILD_VERSION.zip /build/$ARCH_NAME.zip
