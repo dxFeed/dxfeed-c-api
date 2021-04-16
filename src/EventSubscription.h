267db6db2b110c5507bdff4e71ae549d35e021e0
ttldtor <ttldtor@gmail.com>
2021-04-16 18:57:09 +0300

    [DXFC-224] A race condition occurred while reading the file (#53)

--- src/EventSubscription.h
+++ src/EventSubscription.h
@@ -90,6 +90,8 @@ dx_order_source_array_ptr_t dx_get_order_source(dxf_subscription_t subscr_id);
 /* Functions for working with symbols*/
 dxf_ulong_t dx_symbol_name_hasher(dxf_const_string_t symbol_name);
 
+int dx_has_any_subscribed_symbol(dxf_connection_t connection);
+
 #ifdef __cplusplus
 }
 #endif
