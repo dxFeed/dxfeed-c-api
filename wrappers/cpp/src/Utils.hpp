4863ddc09f25a520bef1b2e9e1c6ef9173e6d181
Anatoly Kalin <ttldtor@gmail.com>
2021-06-22 23:55:21 +0300

    fmt 8.0.0

--- wrappers/cpp/src/Utils.hpp
+++ wrappers/cpp/src/Utils.hpp
@@ -24,27 +24,17 @@
 #include <ctime>
 #include <iomanip>
 #include <fmt/format.h>
+#include <fmt/chrono.h>
 
 inline std::string formatTime(long long timestamp, const std::string& format = "%Y-%m-%d %H:%M:%S") {
 const std::size_t TIME_STRING_BUFFER_SIZE = 512;
-	std::stringstream ss;
-	auto t = static_cast<std::time_t>(timestamp);
-	char timeString[TIME_STRING_BUFFER_SIZE];
-
-	std::strftime(timeString, TIME_STRING_BUFFER_SIZE, format.c_str(), gmtime(&t));
-
-	ss << timeString;
-
-	return ss.str();
+	return fmt::format(fmt::format("{{:{}}}", format), fmt::gmtime(static_cast<std::time_t>(timestamp)));
 }
 
 inline std::string formatTimestampWithMillis(long long timestamp) {
 	long long ms = timestamp % 1000;
-	std::stringstream ss;
 
-	ss << formatTime(timestamp / 1000);
-
-	return fmt::format("{}.{:0>3}", ss.str(), ms);
+	return fmt::format("{}.{:0>3}", formatTime(timestamp / 1000), ms);
 }
 
 inline std::string orderScopeToString(dxf_order_scope_t scope) {
