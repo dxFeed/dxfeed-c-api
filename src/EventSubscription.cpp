6280b99100ed4bfb05bc6dec0963e5cc1c4406a8
Anatoly Kalin <ttldtor@gmail.com>
2021-08-19 20:30:33 +0300

    [DXFC-238] Add the ability to set PriceLevelBook symbol or orders sorces

--- src/EventSubscription.cpp
+++ src/EventSubscription.cpp
@@ -37,8 +37,6 @@ extern "C" {
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
-#include <utility>
-#include <vector>
 
 #include "Configuration.hpp"
 #include "EventSubscription.hpp"
