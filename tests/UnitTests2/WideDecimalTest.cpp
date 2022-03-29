7aa993f26435834e4817565af3b88f4d73b58464
Anatoly Kalin <ttldtor@gmail.com>
2022-03-29 15:13:59 +0300

    [DXFC-334] Precision error when converting WideDecimal to Double

--- tests/UnitTests2/WideDecimalTest.cpp
+++ tests/UnitTests2/WideDecimalTest.cpp
@@ -7,7 +7,7 @@
 #include <cstdlib>
 
 namespace WideDecimalTest {
-inline static dxf_long_t MAX_SIGNIFICAND = 36028797018963967LL;
+inline static dxf_long_t MAX_SIGNIFICAND = dx::rightShift(std::numeric_limits<dxf_long_t>::max(), 8);
 inline static dxf_int_t MAX_RANK = 255;
 }  // namespace WideDecimalTest
 
