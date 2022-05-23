dcfe44c935b55a159a12db8310114eeceadd3710
ttldtor <ttldtor@gmail.com>
2022-05-23 15:33:40 +0300

    DXFC-329

---
+++ src/Snapshot/Snapshot.h
@@ -0,0 +1,36 @@
+/*
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Initial Developer of the Original Code is Devexperts LLC.
+ * Portions created by the Initial Developer are Copyright (C) 2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ */
+
+#pragma once
+
+#include <DXTypes.h>
+#include <EventData.h>
+
+#include "../PrimitiveTypes.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ERRORCODE dx_create_snapshot_v2(dxf_connection_t connection, dx_event_id_t event_id, dxf_const_string_t symbol,
+								const char *source, dxf_long_t time, OUT dxf_snapshot_t *snapshot);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/Snapshot/Snapshot.hpp
@@ -0,0 +1,69 @@
+/*
+* The contents of this file are subject to the Mozilla Public License Version
+* 2.0 (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+*
+* Software distributed under the License is distributed on an "AS IS" basis,
+* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+* for the specific language governing rights and limitations under the
+* License.
+*
+* The Initial Developer of the Original Code is Devexperts LLC.
+* Portions created by the Initial Developer are Copyright (C) 2010
+* the Initial Developer. All Rights Reserved.
+*
+* Contributor(s):
+*
+*/
+
+#pragma once
+
+extern "C" {
+#include <DXFeed.h>
+#include <EventData.h>
+}
+
+#include <unordered_map>
+#include <memory>
+#include <optional.hpp>
+
+namespace dx {
+
+struct Snapshot {
+
+};
+
+
+struct SnapshotKey {
+
+};
+
+template <typename ConnectionKey, typename SnapshotKey>
+struct SnapshotManager {
+	static std::unordered_map<ConnectionKey, std::shared_ptr<SnapshotManager>> managers;
+	std::unordered_map<SnapshotKey, std::shared_ptr<Snapshot>> snapshots;
+
+	static std::shared_ptr<SnapshotManager> getInstance(const ConnectionKey& connectionKey) {
+		auto found = managers.find(connectionKey);
+
+		if (found != managers.end()) {
+			return found->second;
+		}
+
+		auto inserted = managers.emplace(connectionKey, std::make_shared<SnapshotManager>());
+
+		if (inserted.second) {
+			return inserted.first->second;
+		}
+
+		return managers[connectionKey];
+	}
+
+	std::shared_ptr<Snapshot> get(const SnapshotKey& snapshotKey) {
+		return nullptr;
+	}
+};
+
+
+}
