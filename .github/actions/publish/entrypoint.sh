5d8df02a3a77507b29a7099893ab94534b2008bf
ttldtor <ttldtor@gmail.com>
2022-12-12 14:30:33 +0300

    Fix ghr

--- .github/actions/publish/entrypoint.sh
+++ .github/actions/publish/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-if [ "${GITHUB_REF}" == "${GITHUB_REF#refs/tags/}" ]; then
+if [ "${GITHUB_REF}" = "${GITHUB_REF#refs/tags/}" ]; then
   echo "This is not a tagged push." 1>&2
   exit 78
 fi
@@ -16,7 +16,7 @@ echo "TAG: $TAG"
 RELEASE_TITLE="$RELEASE_PREFIX$TAG"
 echo "TITLE: $RELEASE_TITLE"
 
-ls -1 $@
+ls -1 "$@"
 
 PRERELEASE_ARG=""
 if [ -n "${INPUT_PRERELEASE_REGEX}" ]; then
@@ -32,10 +32,10 @@ if [ -n "${INPUT_DRAFT_REGEX}" ]; then
   fi
 fi
 
-echo $PATH
+echo "$PATH"
 
 # recreate release
-ghr -u "${GITHUB_REPOSITORY%/*}" -r "${GITHUB_REPOSITORY#*/}" $DRAFT_ARG $PRERELEASE_ARG -replace -delete -n "$RELEASE_TITLE" "$TAG" $@
+ghr -u "${GITHUB_REPOSITORY%/*}" -r "${GITHUB_REPOSITORY#*/}" $DRAFT_ARG $PRERELEASE_ARG -replace -delete -n "$RELEASE_TITLE" "$TAG" "$@"
 
 #
 # update release notes
@@ -45,11 +45,11 @@ if [ -n "${INPUT_NOTES}" ]; then
     DATA=$(jq -n --arg message "$INPUT_NOTES" '{"body":$message}')
     echo "$DATA"
 
-    RELEASE_ID=$(curl --silent -X 'GET' https://api.github.com/repos/${GITHUB_REPOSITORY%/*}/${GITHUB_REPOSITORY#*/}/releases/tags/${TAG} | jq ".id")
+    RELEASE_ID=$(curl --silent -X 'GET' https://api.github.com/repos/"${GITHUB_REPOSITORY%/*}"/"${GITHUB_REPOSITORY#*/}"/releases/tags/"${TAG}" | jq ".id")
     echo "RELEASE_ID: $RELEASE_ID"
 
     echo "API URL: https://api.github.com/repos/${GITHUB_REPOSITORY%/*}/${GITHUB_REPOSITORY#*/}/releases/$RELEASE_ID?access_token=${GITHUB_TOKEN}"
-    curl --silent -X 'PATCH' https://api.github.com/repos/${GITHUB_REPOSITORY%/*}/${GITHUB_REPOSITORY#*/}/releases/$RELEASE_ID?access_token=${GITHUB_TOKEN} -d "$DATA"
+    curl --silent -X 'PATCH' https://api.github.com/repos/"${GITHUB_REPOSITORY%/*}"/"${GITHUB_REPOSITORY#*/}"/releases/"$RELEASE_ID"?access_token="${GITHUB_TOKEN}" -d "$DATA"
 else
   echo ""
   echo "Release notes not found for $TAG. Won't update release description."
