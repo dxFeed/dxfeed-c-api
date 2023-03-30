956ec7ff351fa83aa5be838bff965c84607dd89f
Anatoly Kalin <ttldtor@gmail.com>
2023-03-30 23:57:40 +0300

    Release 8.8.2

--- src/Version.h
+++ src/Version.h
@@ -28,7 +28,7 @@
 
 #define DX_VERSION_MAJOR 8
 #define DX_VERSION_MINOR 8
-#define DX_VERSION_PATCH 1
+#define DX_VERSION_PATCH 2
 
 #define DX_VER_FILE_VERSION         DX_VERSION_MAJOR, DX_VERSION_MINOR, DX_VERSION_PATCH
 #define DX_VER_FILE_VERSION_STR     DX_TO_STRING(DX_VERSION_MAJOR.DX_VERSION_MINOR.DX_VERSION_PATCH)
@@ -46,7 +46,7 @@
 #define DX_VER_LEGAL_COPYRIGHT   "Copyright (C) 2010-2023 Devexperts LLC"
 #define DX_VER_PRODUCT_NAME      "dxFeed C API framework"
 
-#define DX_LIBRARY_VERSION L"DXFeed.cpp v 8.8.1 (c) Devexperts LLC"
+#define DX_LIBRARY_VERSION L"DXFeed.cpp v 8.8.2 (c) Devexperts LLC"
 
 extern dxf_const_string_t DX_LIBRARY_OPTIONS;
 
