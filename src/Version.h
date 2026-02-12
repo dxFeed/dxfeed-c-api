299eced3327deef995694d9e7c65319096f6e3e8
AnatolyKalin <anatoly.kalin@devexperts.com>
2026-02-12 13:49:46 +0300

    Release 9.0.0

--- src/Version.h
+++ src/Version.h
@@ -26,9 +26,9 @@
 #define DX_STRINGIFY(x) #x
 #define DX_TO_STRING(x) DX_STRINGIFY(x)
 
-#define DX_VERSION_MAJOR 8
-#define DX_VERSION_MINOR 8
-#define DX_VERSION_PATCH 3
+#define DX_VERSION_MAJOR 9
+#define DX_VERSION_MINOR 0
+#define DX_VERSION_PATCH 0
 
 #define DX_VER_FILE_VERSION         DX_VERSION_MAJOR, DX_VERSION_MINOR, DX_VERSION_PATCH
 #define DX_VER_FILE_VERSION_STR     DX_TO_STRING(DX_VERSION_MAJOR.DX_VERSION_MINOR.DX_VERSION_PATCH)
@@ -46,7 +46,7 @@
 #define DX_VER_LEGAL_COPYRIGHT   "Copyright (C) 2010-2025 Devexperts LLC"
 #define DX_VER_PRODUCT_NAME      "dxFeed C API framework"
 
-#define DX_LIBRARY_VERSION L"DXFeed.cpp v 8.8.3 (c) Devexperts LLC"
+#define DX_LIBRARY_VERSION L"DXFeed.cpp v 9.0.0 (c) Devexperts LLC"
 
 extern dxf_const_string_t DX_LIBRARY_OPTIONS;
 
